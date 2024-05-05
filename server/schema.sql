e647eebff2951cca13b63487c507753183300a07
Erick <efuentes.edf9@gmail.com>
2024-05-04 22:37:49 -0400

    JOIN con las tablas share and all to-do

--- server/schema.sql
+++ server/schema.sql
@@ -21,11 +21,35 @@ CREATE TABLE shared_todos(
     id INT AUTO_INCREMENT PRIMARY KEY,
     todo_id INT,
     user_id INT,
-    FOREIGN KEY (todo_id) REFERENCES todos (id),
-    FOREIGN KEY (user_id) REFERENCES users (id)
+    share_with_id INT,
+    FOREIGN KEY (todo_id) REFERENCES todos (id) ON DELETE CASCADE,
+    FOREIGN KEY (user_id) REFERENCES todos (id) ON DELETE CASCADE,
+    FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
 );
 
 --Insert two users into the users table
 INSERT INTO users (name, email, password) VALUES ('Erick', 'user1@example.com','password1');
 INSERT INTO users (name, email, password) VALUES ('Erick', 'user2@example.com','password2');
 INSERT INTO users (name, email, password) VALUES ('Joudy', 'user3@example.com','password3');
+
+--INSERT TODOS INTO THE TODOS TABLE
+INSERT INTO todos (title, user_id) 
+VALUES
+("🏃🏽‍♂️ Go for a morning run", 1),
+("💻 Work on project presentation", 1),
+("🛒 Go grocery shopping", 1),
+("📚 Read 30 pages of book programing", 1),
+("🚴🏼 Ride bike to the park", 1),
+("🍱 Cook dinner for family", 1),
+("🧘🏽‍♂️ Practice yoga", 1),
+("🎧 Listen to a podcast", 1),
+("🧹 Clean the house", 1),
+("🛌🏽 Get 8 hours of sleep", 1);
+
+INSERT INTO shared_todos(todo_id, user_id, share_with_id)
+VALUES (1, 1, 4);
+
+SELECT todos.*, shared_todos.share_with_id
+FROM todos
+LEFT JOIN shared_todos ON todos.id = shared_todos.todo_id
+WHERE todos.user_id = [user_id] OR shared_todos.share_with_id = [user_id];
